812c819ba3d232f1b4a260a5ecf9e0927e09dfdd
João <joaovitorsenac7817@gmail.com>
2024-06-06 13:46:38 -0300

    feat: create home static

--- App.js
+++ App.js
@@ -1,7 +1,69 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Slider from '@react-native-community/slider';
+
+import Logo from './src/assets/logo.png'
 
 export default function App() {
   return (
-    <Text>Olá Mundo</Text>
+    <View style={styles.container}>
+      <Image source={ Logo } style={styles.logo}/>
+
+      <Text style={styles.title}>20 caracteres</Text>
+    
+      <View style={styles.area}>
+        <Slider 
+          style={{ height: 50 }}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor='#ff0000'
+          minimumTrackTintColor='#000'
+          thumbTintColor='#392de9'
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>GERAR SENHA</Text>
+      </TouchableOpacity>
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F3FF',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    marginBottom: 60,
+  }, 
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold'
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    padding: 8,
+    width: '80%',
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+  }, 
+  button: {
+    backgroundColor: '#392de9',
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: '500',
+    letterSpacing: 1.2
+  }
+});
